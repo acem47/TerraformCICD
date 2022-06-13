5d8dca9d955649c1d382474fac711c414b485a99
acem47 <62121871+acem47@users.noreply.github.com>
2022-06-14 01:57:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,45 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
+	version = "~>2.5.0"
+	features{}
 }
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraformStateRG"
-    storage_account_name = "terraformstfl"
+    resource_group_name  = "terraformStateRG.terraformStateRG.name"
+    storage_account_name = "terraformstfl.terraformStateRG.name"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
 
-
-resource "azurerm_resource_group" "rg" {
-  name     = "moe-tes-grp"
-  location = "uaenorth"
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
 }
- 
+
+resource "azurerm_resource_group" "dockerdevops" {
+  name     = "dockerdevops"
+  location = "UAE North"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "node-group-moe"
+  location            = azurerm_resource_group.dockerdevops.location
+  resource_group_name = azurerm_resource_group.dockerdevops.name
+
+  ip_address_type = "public"
+  dns_name_label  = "moe78"
+  os_type         = "Linux"
+
+  container {
+    name   = "node-moe"
+    image  = "moe78/nodetest:${var.imagebuild}"
+    cpu    = "1"
+    memory = "1"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+} 

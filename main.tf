0cd89f494df9c234fe5675084a49e629f456b9a5
acem47 <62121871+acem47@users.noreply.github.com>
2022-06-14 02:08:04 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,15 @@ variable "imagebuild" {
   description = "Latest Image Build"
 }
 
-resource "azurerm_resource_group" "moe-tes-grp" {
+resource "azurerm_resource_group" "moe_test_RG" {
   name     = "dockerdevops"
   location = "UAE North"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
   name                = "node-group-moe"
-  location            = azurerm_resource_group.moe-tes-grp.location
-  resource_group_name = azurerm_resource_group.moe-tes-grp.name
+  location            = azurerm_resource_group.moe_test_RG.location
+  resource_group_name = azurerm_resource_group.moe_test_RG.name
 
   ip_address_type = "public"
   dns_name_label  = "moe78"
